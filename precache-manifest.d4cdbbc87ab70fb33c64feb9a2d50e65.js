b26f4933f97c7dcf1f950eafd73005302fa7f378
Dolon Pantheon <dolonishamandal@gmail.com>
2020-07-03 18:26:37 +0530

    Updates

---
+++ precache-manifest.d4cdbbc87ab70fb33c64feb9a2d50e65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc90a6b03a9e19d55f7b68428f0b8ec",
+    "url": "/ReactFoodies/index.html"
+  },
+  {
+    "revision": "cb914c1d4e1e657d2dff",
+    "url": "/ReactFoodies/static/css/main.745d0c73.chunk.css"
+  },
+  {
+    "revision": "a4122b26e77149718916",
+    "url": "/ReactFoodies/static/js/2.bebb81b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactFoodies/static/js/2.bebb81b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb914c1d4e1e657d2dff",
+    "url": "/ReactFoodies/static/js/main.bf47bc9a.chunk.js"
+  },
+  {
+    "revision": "2885da33eaa6c137dc1f",
+    "url": "/ReactFoodies/static/js/runtime-main.066142de.js"
+  }
+]);
